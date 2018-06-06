4e8cdc0d48a45f6cd363b3c58da871398e3782f4
Stephen Fraser <stephenwf@users.noreply.github.com>
2018-06-06 23:39:02 +0100

    Performance regressions (2) (#87)

--- packages/iiif-redux/src/utility/preloadNormalizedList.js
+++ packages/iiif-redux/src/utility/preloadNormalizedList.js
@@ -1,9 +1,15 @@
 import { normalize } from '../schema/presentation2';
 
 export default async function preloadNormalizedList(list) {
+  let proms = [];
+
   for (const listItem of list) {
-    await new Promise(resolve => {
-      setTimeout(() => resolve(normalize(listItem)), 0);
-    });
+    proms.push(
+      new Promise(resolve => {
+        setTimeout(() => resolve(normalize(listItem)), 0);
+      })
+    );
   }
+
+  await Promise.all(proms);
 }
